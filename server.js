890aeeca8860455e16cafb89b7d474c9c12b18d8
Harsh <mishraharsh9569@gmail.com>
2024-07-24 12:16:11 +0530

    initial commit

--- server.js
+++ server.js
@@ -3,41 +3,48 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 
-// Initialize the app
 const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect('mongodb://localhost/todo-app', { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://mishraharsh9569:harsh%401234@lucky-cluster.lq2nwkh.mongodb.net/todolist?retryWrites=true&w=majority&appName=Lucky-Cluster', { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.log(err));
 
-// Define the To-Do model
 const TodoSchema = new mongoose.Schema({
   text: { type: String, required: true }
 });
 const Todo = mongoose.model('Todo', TodoSchema);
 
-// API endpoints
 app.get('/todos', async (req, res) => {
-  const todos = await Todo.find();
-  res.json(todos);
+  try {
+    const todos = await Todo.find();
+    res.json(todos);
+  } catch (err) {
+    res.status(500).send({ message: 'Error fetching todos', error: err });
+  }
 });
 
 app.post('/todos', async (req, res) => {
-  const newTodo = new Todo({
-    text: req.body.text
-  });
-  await newTodo.save();
-  res.json(newTodo);
+  try {
+    const newTodo = new Todo({
+      text: req.body.text
+    });
+    await newTodo.save();
+    res.json(newTodo);
+  } catch (err) {
+    res.status(500).send({ message: 'Error adding todo', error: err });
+  }
 });
 
 app.delete('/todos/:id', async (req, res) => {
-  const result = await Todo.findByIdAndDelete(req.params.id);
-  res.json(result);
+  try {
+    const result = await Todo.findByIdAndDelete(req.params.id);
+    res.json(result);
+  } catch (err) {
+    res.status(500).send({ message: 'Error deleting todo', error: err });
+  }
 });
 
-// Start the server
 const port = 5000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
